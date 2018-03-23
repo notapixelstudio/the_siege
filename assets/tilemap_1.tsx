97da92f6f75b5bdd58bdf505003939f38f97088f
c.difranco <c.difranco@sssup.it>
2018-03-23 22:18:34 +0100

    added .tsx file

---
+++ assets/tilemap_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tilemap_1" tilewidth="16" tileheight="16" tilecount="81" columns="9">
+ <image source="tilemap_1.png" trans="00a2e8" width="144" height="144"/>
+</tileset>
