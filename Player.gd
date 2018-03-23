f02143e8fa87747d9800517c8af182950edb0682
salvob <tholalak@yahoo.it>
2018-03-23 23:21:05 +0100

    Tutorial grid movement completed and porting to Godot3. Player moves onto the grid. Imported tilemap/tileset

--- Player.gd
+++ Player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 var direction = Vector2()
 
+
 const TOP = Vector2(0, -1)
 const RIGHT = Vector2(1, 0)
 const DOWN = Vector2(0, -1)
@@ -21,46 +22,41 @@ var type
 var grid
 
 
-
 func _ready():
-	grid = get_parent()
+	grid = get_parent().get_node("GridMap")
+	type = grid.PLAYER
 
 	
 func _physics_process(delta):
 	direction = Vector2()
 	speed = 0
 	
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("ui_up"):
 		direction.y = -1
-	elif Input.is_action_pressed("move_down"):
+	elif Input.is_action_pressed("ui_down"):
 		direction.y = 1
 
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("ui_left"):
 		direction.x = -1
-	elif Input.is_action_pressed("move_right"):
+	elif Input.is_action_pressed("ui_right"):
 		direction.x = 1
 
 	if not is_moving and direction != Vector2():
-		target_direction = direction.normalized()
-		if grid.is_cell_vacant(get_pos(), direction):
-			target_pos = grid.update_child_pos(get_pos(), direction, type)
+		target_direction = direction
+		if grid.is_cell_vacant(position, target_direction):
+			target_pos = grid.update_child_pos(self)
 			is_moving = true
 	elif is_moving:
 		speed = MAX_SPEED
-		velocity = speed * target_direction * delta
-
-		var pos = get_pos()
-		var distance_to_target = pos.distance_to(target_pos)
-		var move_distance = velocity.length()
-
-		if move_distance > distance_to_target:
-			velocity = target_direction * distance_to_target
+		velocity = speed * target_direction
+		var pos = position
+		var distance_to_target = Vector2(abs(target_pos.x - position.x), abs(target_pos.y - pos.y))
+		if abs(velocity.x) > distance_to_target.x: 
+			velocity.x = distance_to_target.x * target_direction.x
 			is_moving = false
 
+		if abs(velocity.y) > distance_to_target.y: 
+			velocity.y = distance_to_target.y * target_direction.y
+			is_moving = false
 		move_and_collide(velocity)
-
-	
-	pass
-	
-func _input(event):
 	
