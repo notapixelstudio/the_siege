fd38417f8fe037d3e5751b43959f4431a2d9890c
c.difranco <c.difranco@sssup.it>
2018-03-25 00:43:26 +0100

    first version with new UI but with no attack

--- UI.gd
+++ UI.gd
@@ -1,20 +1,23 @@
 extends Node
 
-var cards = []
+var btn_commander
+var btn_carpenter
+var btn_wizard
+
+var btn_cards = []
+
 var path_cards = ["res://assets/cards/commander_card_back.png", "res://assets/cards/carpenter_card_back.png" , "res://assets/cards/wizard_card_back.png"]
 func _ready():
 
-	var btn_commander = get_node("TextureCommander")
-	var btn_carpenter = get_node("TextureCarpenter")
-	var btn_wizard = get_node("TextureWizard")
+	btn_commander = get_node("TextureCommander")
+	btn_carpenter = get_node("TextureCarpenter")
+	btn_wizard = get_node("TextureWizard")
 
 	btn_commander.connect("pressed",get_node("/root/Game"),"_on_btn_commander_pressed")
 	btn_carpenter.connect("pressed",get_node("/root/Game"),"_on_btn_carpenter_pressed")
 	btn_wizard.connect("pressed",get_node("/root/Game"),"_on_btn_wizard_pressed")
 	
-	# TODO: need for every card action
-	var btn_action = get_node("TextureCard1")
-	btn_action.connect("pressed", get_node("/root/Game"),"_on_btn_action_pressed")
+
 
 func do_show_popup_counselor(i,name):
 	var text = "The "+ name +" requires to talk with ... (choose a counselour)"
@@ -51,6 +54,7 @@ func do_show_cards(regnant,counselor):
 		card2 = get_node("TextureCard5")
 		card3 = get_node("TextureCard6")
 		
+
 	# show them
 	card1.show()
 	card2.show()
@@ -61,11 +65,57 @@ func do_show_cards(regnant,counselor):
 	card2.texture_normal = load(path_cards[counselor.id])
 	card3.texture_normal = load(path_cards[counselor.id])
 	
-	# store cards
-	cards.append(card1)
-	cards.append(card2)
-	cards.append(card3)
+	if (counselor.id == 0):
+		card1.connect("pressed",get_node("/root/Game"),"_on_btn_attackcommander_pressed")
+		card2.connect("pressed",get_node("/root/Game"),"_on_btn_attackcommander_pressed")
+		card3.connect("pressed",get_node("/root/Game"),"_on_btn_attackcommander_pressed")
+	elif(counselor.id == 1):
+		card1.connect("pressed",get_node("/root/Game"),"_on_btn_attackcarpenter_pressed")
+		card2.connect("pressed",get_node("/root/Game"),"_on_btn_attackcarpenter_pressed")
+		card3.connect("pressed",get_node("/root/Game"),"_on_btn_attackcarpenter_pressed")
+	elif(counselor.id == 3):
+		card1.connect("pressed",get_node("/root/Game"),"_on_btn_attackwizard_pressed")
+		card2.connect("pressed",get_node("/root/Game"),"_on_btn_attackwizard_pressed")
+		card3.connect("pressed",get_node("/root/Game"),"_on_btn_attackwizard_pressed")
 	
+
+	
+ 
+	
+	btn_cards.append(card1);
+	btn_cards.append(card2);
+	btn_cards.append(card3);
+	
+	
+	
+	
+func disable_counsellors():	
+	btn_commander.disabled = true
+	btn_carpenter.disabled = true
+	btn_wizard.disabled = true 
+	
+func enable_counsellors():	
+	btn_commander.disabled = false
+	btn_carpenter.disabled = false
+	btn_wizard.disabled = false
+ 
+func disable_all_cards():
+	for card in btn_cards:
+		card.disabled = true
+	
+func enable_all_cards():
+	for card in btn_cards:
+		card.disabled = false
+		
+		
+func hide_all_cards():
+	for card in btn_cards:
+		card.hide()
+		
+func show_all_cards():
+	for card in btn_cards:
+		card.show()
+
 	
 func update_ui(this_round,this_turn):
 	var string = this_turn + " turn: #" + str(this_round)
@@ -77,5 +127,11 @@ func do_flip_cards(cards):
 	for card in cards:
 		get_node("TextureCard"+str(i)).texture_normal = load(card.res_front)
 		i+=1
-	pass
+	enable_all_cards()
+	
+		
+func reset_cards():
+	
+	btn_cards = []
+	
 	
