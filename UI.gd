778a31477944f3bf9532309f106d27ea81b91045
c.difranco <c.difranco@sssup.it>
2018-03-24 14:50:44 +0100

    chosen the counselor, and improving communication with UI

--- UI.gd
+++ UI.gd
@@ -1,12 +1,31 @@
 extends Node
 
 func _ready():
-	pass
+
+	var btn_commander = get_node("pickCounselor/VBoxContainer/btn_commander")
+	var btn_carpenter = get_node("pickCounselor/VBoxContainer/btn_carpenter")
+	var btn_wizard = get_node("pickCounselor/VBoxContainer/btn_wizard")
+
+	btn_commander.connect("pressed",get_node("/root/Game"),"_on_btn_commander_pressed")
+	btn_carpenter.connect("pressed",get_node("/root/Game"),"_on_btn_carpenter_pressed")
+	btn_wizard.connect("pressed",get_node("/root/Game"),"_on_btn_wizard_pressed")
+	get_node("pickCounselor").hide()
 
 func do_show_popup_counselor(i,name):
 	
-	print("The " + name + " summons")
+	print("UI: do_show_popup_counselor, by the " + name)
 	get_node("pickCounselor/VBoxContainer/title").text = "The " + name + " summons:"
 	get_node("pickCounselor/VBoxContainer/id").text = str(i);
 	get_node("pickCounselor").show();
+	 
+func do_show_cards():
+	print("UI: do_show_cards, counselor " )
+	 
+	
+func update_ui(this_round,this_turn):
+	
+	var string = this_turn + " turn: #" + str(this_round)
+	print("UI: printed ->" + string) 
+	get_node("turn_label").text = string
+	
 	 
