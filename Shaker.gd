8e2778d869c99f71352e0d3d7d8b437d6fd72c89
salvob <tholalak@yahoo.it>
2018-03-25 22:00:46 +0200

    trying effects

---
+++ Shaker.gd
@@ -0,0 +1,14 @@
+extends Node
+
+const duration = 0.18
+const magnitude = 3
+
+var camera = get_parent()
+
+func _ready():
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
